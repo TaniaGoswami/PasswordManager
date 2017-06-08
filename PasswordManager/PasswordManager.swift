ae66d568324787c9d824ae867967a0626e672d66
Tania Goswami <tania.goswami@compass.com>
2017-06-08 12:32:35 -0400

    Added methods for native app login

---
+++ PasswordManager/PasswordManager.swift
@@ -0,0 +1,78 @@
+//  Copyright © 2017 Compass. All rights reserved.
+
+import Foundation
+import UIKit
+import MobileCoreServices
+
+public enum PasswordManagerError: Error {
+    case usernameNotFound
+    case passwordNotFound
+    case extensionCancelledByUser
+    case extensionNoAttachments
+    case extensionUnexpectedData
+}
+
+class PasswordManager {
+    private let versionNumber = 184
+    static let shared = PasswordManager()
+    private let passwordManagerAvailable: URL = {
+        return URL(string: "org-appextension-feature-password-management://")!
+    }()
+
+    private init() {}
+
+    var isPasswordManagerAvailable: Bool {
+        return UIApplication.shared.canOpenURL(passwordManagerAvailable)
+    }
+
+    func findLogin(urlString: String, viewController: UIViewController, sender: UIView, completion: @escaping ([String: Any]?, Error?) -> Void) {
+        let item: [String: Any] = ["version_number": versionNumber, "url_string": urlString]
+        let activityViewController = activityViewControllerForItem(item: item, viewController: viewController, sender: sender, typeIdentifier: "org.appextension.find-login-action")
+        activityViewController.completionWithItemsHandler = { [weak self] activityType, completed, returnedItems, activityError in
+            guard let item = returnedItems?.first as? NSExtensionItem else {
+                let error = activityError ?? PasswordManagerError.extensionCancelledByUser
+                completion(nil, error)
+                return
+            }
+
+            self?.processExtensionItem(extensionItem: item, completion: { itemDictionary, error in
+                DispatchQueue.main.async {
+                    completion(itemDictionary, error)
+                }
+            })
+        }
+        viewController.present(activityViewController, animated: true, completion: nil)
+    }
+
+    private func activityViewControllerForItem(item: [AnyHashable: Any], viewController: UIViewController, sender: UIView, typeIdentifier: String) -> UIActivityViewController {
+        let itemProvider = NSItemProvider(item: item as NSSecureCoding, typeIdentifier: typeIdentifier)
+        let extensionItem = NSExtensionItem()
+        extensionItem.attachments = [itemProvider]
+
+        let activityViewController = UIActivityViewController(activityItems: [extensionItem], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = sender
+
+        return activityViewController
+    }
+
+    private func processExtensionItem(extensionItem: NSExtensionItem?, completion: @escaping ([String: Any]?, Error?) -> Void) {
+        guard let itemProvider = extensionItem?.attachments?.first as? NSItemProvider else {
+            completion(nil, PasswordManagerError.extensionNoAttachments)
+            return
+        }
+
+        let propertyListType = kUTTypePropertyList as String
+        if !itemProvider.hasItemConformingToTypeIdentifier(propertyListType) {
+            completion(nil, PasswordManagerError.extensionUnexpectedData)
+            return
+        }
+
+        itemProvider.loadItem(forTypeIdentifier: propertyListType, options: nil, completionHandler: { secureCodingItem, _ in
+            guard let itemDictionary = secureCodingItem as? [String: Any] else {
+                completion(nil, PasswordManagerError.extensionUnexpectedData)
+                return
+            }
+            completion(itemDictionary, nil)
+        })
+    }
+}
