e3fb6c996c9aaf5610fc20311ddb4bd572a5044c
Tania Goswami <tania.goswami@compass.com>
2017-06-08 11:36:29 -0400

    Initial commit

---
+++ PasswordManager/PasswordManager.h
@@ -0,0 +1,19 @@
+//
+//  PasswordManager.h
+//  PasswordManager
+//
+//  Created by Tania Goswami on 6/8/17.
+//  Copyright © 2017 Compass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PasswordManager.
+FOUNDATION_EXPORT double PasswordManagerVersionNumber;
+
+//! Project version string for PasswordManager.
+FOUNDATION_EXPORT const unsigned char PasswordManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasswordManager/PublicHeader.h>
+
+
